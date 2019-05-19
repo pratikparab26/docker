805a1508cc3745dddaedc99ffea0fca62342bf34
ritkaluskar <ritkaluskar@gmail.com>
2019-05-20 02:43:04 +0530

    added_jenkins job

--- HelloWorld_job.groovy
+++ HelloWorld_job.groovy
@@ -1,31 +1,137 @@
-import jenkins.model.Jenkins
+import static jenkins.model.Jenkins.instance as jenkins
 
-def jobDSL="""
-node {
-  stage("test"){
-   echo 'Hello World'
-  }
+import com.cloudbees.hudson.plugins.folder.computed.DefaultOrphanedItemStrategy
+import com.cloudbees.jenkins.plugins.bitbucket.BitbucketSCMNavigator
+import com.cloudbees.jenkins.plugins.bitbucket.OriginPullRequestDiscoveryTrait
+import com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait
+import com.cloudbees.jenkins.plugins.bitbucket.ForkPullRequestDiscoveryTrait
+
+import jenkins.branch.NoTriggerOrganizationFolderProperty
+import jenkins.branch.OrganizationFolder
+import jenkins.scm.api.mixin.ChangeRequestCheckoutStrategy
+import jenkins.scm.impl.trait.RegexSCMHeadFilterTrait
+import jenkins.scm.impl.trait.RegexSCMSourceFilterTrait
+
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProjectFactory
+
+// Pull the name of the instance from environment
+def name = System.getenv('JENKINS_NAME')
+def displayName = name.capitalize()
+def owner = name.toUpperCase()
+
+println("--> Setting up Core Jobs Jenkins J20 server ${name}...")
+
+// Define parameters for all the core jobs we want to create
+core_jobs = [ new Expando(jobname: "${name}-ci",
+        displayName: "${displayName} CI",
+        owner: "${owner}",
+        description: "Component Continuous Integration Builds for Bitbucket Project ${owner}",
+        jenkinsfile: "Jenkinsfile",
+        branchFilter: ".*",
+        autoBuild: ".*"),
+              new Expando(jobname: "${name}-release",
+                      displayName: "${displayName} Release",
+                      owner: "${owner}",
+                      description: "Component Release Builds for Bitbucket Project ${owner}",
+                      jenkinsfile: "Jenkinsfile.release",
+                      branchFilter: "master",
+                      autoBuild: ""),
+              new Expando(jobname: "${name}-docker-ci-build",
+                      displayName: "${displayName} Docker CI Build",
+                      owner: "${owner}",
+                      description: "Docker Continuous Integration Builds for Bitbucket Project ${owner}",
+                      jenkinsfile: "Jenkinsfile.docker.ci",
+                      branchFilter: ".*",
+                      autoBuild: ""),
+              new Expando(jobname: "${name}-docker-ci-deploy",
+                      displayName: "${displayName} Docker CI Deploy",
+                      owner: "${owner}",
+                      description: "Docker Continuous Integration Deploys for Bitbucket Project ${owner}",
+                      jenkinsfile: "Jenkinsfile.docker.ci.deploy",
+                      branchFilter: ".*",
+                      autoBuild: ""),
+              new Expando(jobname: "${name}-docker-release-build",
+                      displayName: "${displayName} Docker Release Build",
+                      owner: "${owner}",
+                      description: "Docker Release Builds for Bitbucket Project ${owner}",
+                      jenkinsfile: "Jenkinsfile.docker.release",
+                      branchFilter: "master",
+                      autoBuild: ""),
+              new Expando(jobname: "${name}-docker-release-deploy",
+                      displayName: "${displayName} Docker Release Deploy",
+                      owner: "${owner}",
+                      description: "Docker Release Deploys for Bitbucket Project ${owner}",
+                      jenkinsfile: "Jenkinsfile.docker.release.deploy",
+                      branchFilter: "master",
+                      autoBuild: ""),
+]
+
+// Get this once to check for existing jobs
+jobs = jenkins.getAllItems()
+
+for (core_job in core_jobs) {
+
+    println '----> checking for existence of job ' + core_job.jobname
+
+    def shouldCreate = true
+    // check to see if we've created this job already
+    jobs.each { j ->
+        if (j instanceof jenkins.branch.OrganizationFolder &&
+                j.fullName.contains(core_job.jobname)) {
+            println '----> Already have a job for ' + j.fullName + ' of type:' + j.getClass()
+            shouldCreate = false
+        }
+    }
+
+    // If we found the job by name, move along to the next job
+    if (!shouldCreate) {
+        continue
+    }
+
+    println '----> configuring job ' + core_job.jobname
+
+    // start by creating the toplevel folder
+    def folder = jenkins.createProject(OrganizationFolder, core_job.jobname)
+
+    // Configure the Bitbucket SCM integration
+    def navigator = new BitbucketSCMNavigator(core_job.owner)
+    navigator.credentialsId = "jenkins-bitbucket-login"
+    navigator.serverUrl = "https://bitbucket.example.com"
+    navigator.traits = [
+            //new RegexSCMSourceFilterTrait(core_job.repoFilter),   // build only these repos
+            new RegexSCMHeadFilterTrait(core_job.branchFilter),     // only inspect branches of this form
+            new BranchDiscoveryTrait(3),                            // discover all branches, including PRs
+            new OriginPullRequestDiscoveryTrait(1),                 // Merge
+            new ForkPullRequestDiscoveryTrait([ChangeRequestCheckoutStrategy.MERGE].toSet(),
+                    new ForkPullRequestDiscoveryTrait.TrustEveryone())
+    ]
+    folder.navigators.replace(navigator)
+
+    folder.description = core_job.description
+    folder.displayName = core_job.displayName
+
+    // Delete orphan items after 5 days
+    folder.orphanedItemStrategy = new DefaultOrphanedItemStrategy(true, "5", "")
+
+    // Configure what Jenkinsfile we should be looking for
+    WorkflowMultiBranchProjectFactory factory = new WorkflowMultiBranchProjectFactory()
+    factory.scriptPath = core_job.jenkinsfile
+    folder.projectFactories.replace(factory)
+
+    // configure the repos to automatically build
+    folder.addProperty(new NoTriggerOrganizationFolderProperty(core_job.autoBuild))
+
+    jenkins.save()
+
+    println '----> configured job ' + core_job.jobname
+
+    Thread.start {
+        sleep 3000 // 3 seconds
+        println '----> Running Bitbucket organization scan for job ' + core_job.jobname
+        folder.scheduleBuild()
+    }
+
 }
 
-""";
-//http://javadoc.jenkins.io/plugin/workflow-cps/index.html?org/jenkinsci/plugins/workflow/cps/CpsFlowDefinition.html
-def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition(jobDSL, true);
-//http://javadoc.jenkins.io/jenkins/model/Jenkins.html
-def parent = Jenkins.instance;
-//parent=Jenkins.instance.getItemByFullName("parentFolder/subFolder")
-//http://javadoc.jenkins.io/plugin/workflow-job/org/jenkinsci/plugins/workflow/job/WorkflowJob.html
-def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "testJob")
-job.definition = flowDefinition
-
-job.setConcurrentBuild(false);
-
-//http://javadoc.jenkins.io/plugin/branch-api/jenkins/branch/RateLimitBranchProperty.html
-job.addProperty( new jenkins.branch.RateLimitBranchProperty.JobPropertyImpl
-    (new jenkins.branch.RateLimitBranchProperty.Throttle (60,"hours")));
-def spec = "H 0 1 * *";
-hudson.triggers.TimerTrigger newCron = new hudson.triggers.TimerTrigger(spec);
-newCron.start(job, true);
-job.addTrigger(newCron);
-job.save();
-
-Jenkins.instance.reload()
+println '----> all jobs configured'
+println("--> Setting up Core Jobs Jenkins J20 server ${name} - DONE")
